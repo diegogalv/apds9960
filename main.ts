52467675f07341cc5247deabe19ca4cadddf22fb
diegogalv <123975129+diegogalv@users.noreply.github.com>
2023-02-13 15:50:51 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -46,7 +46,7 @@ function get_Reg_lux(reg: number): number {
 
 
 function getReg_int(reg: number): number {
-    pins.i2cWriteNumber(APDS9960_ADDRESS, reg, NumberFormat.UInt16BE);
+    pins.i2cWriteNumber(APDS9960_ADDRESS, reg, NumberFormat.UInt8BE);
     basic.pause(3)
     return pins.i2cReadNumber(APDS9960_ADDRESS, NumberFormat.Int32LE);
 }
