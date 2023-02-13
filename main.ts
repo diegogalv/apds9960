2b2d79fec76061ea9caf6e095c5a2b3558b48d62
diegogalv <123975129+diegogalv@users.noreply.github.com>
2023-02-13 10:26:24 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,3 @@
-/**
- * makecode APDS9930 Digital Proximity and Ambient Light Sensor package.
- * From microbit/micropython Chinese community.
- * http://www.micropython.org.cn
- */
-
 const APDS9960_ADDRESS = 0x39
 const APDS9960_ENABLE = 0x80
 const APDS9960_ATIME = 0x81
@@ -29,143 +23,144 @@ const APDS9960_GDATAL = 0x98
 const APDS9960_GDATAH = 0x99
 const APDS9960_BDATAL = 0x9A
 const APDS9960_BDATAH = 0x9B
-const APDS9960_PDATA = 0x9C
-const APDS9960_POFFSET_UR = 0x9D
-const APDS9960_POFFSET_DL = 0x9E
-const APDS9960_CONFIG3 = 0x9F
-const APDS9960_GPENTH = 0xA0
-const APDS9960_GEXTH = 0xA1
-const APDS9960_GCONF1 = 0xA2
-const APDS9960_GCONF2 = 0xA3
-const APDS9960_GOFFSET_U = 0xA4
-const APDS9960_GOFFSET_D = 0xA5
-const APDS9960_GOFFSET_L = 0xA7
-const APDS9960_GOFFSET_R = 0xA9
-const APDS9960_GPULSE = 0xA6
-const APDS9960_GCONF3 = 0xAA
-const APDS9960_GCONF4 = 0xAB
-const APDS9960_GFLVL = 0xAE
-const APDS9960_GSTATUS = 0xAF
-const APDS9960_IFORCE = 0xE4
-const APDS9960_PICLEAR = 0xE5
-const APDS9960_CICLEAR = 0xE6
-const APDS9960_AICLEAR = 0xE7
-const APDS9960_GFIFO_U = 0xFC
-const APDS9960_GFIFO_D = 0xFD
-const APDS9960_GFIFO_L = 0xFE
-const APDS9960_GFIFO_R = 0xFF
+
 
 /**
- * set APDS9930's reg
- */
-function set2Reg(command: number): number {
-    let buf = pins.createBuffer(2);
-    // basic.pause(10)
-    // basic.pause(10)
-    buf[1] = command >> 8
-    buf[0] = command & 0xFF
-    return pins.i2cWriteBuffer(APDS9960_ADDRESS, buf)
-}
-
+   * set APDS9930's reg
+   */
 function setReg(reg: number, dat: number): void {
     let _wbuf = pins.createBuffer(2);
-    _wbuf[0] = reg | 0xFF;
+    _wbuf[0] = reg;
     _wbuf[1] = dat;
     pins.i2cWriteBuffer(APDS9960_ADDRESS, _wbuf);
 }
 
-function read_buf_8(cmd: number): number {
-    let i2cbuf = pins.i2cReadBuffer(APDS9960_ADDRESS, pins.sizeOf(NumberFormat.UInt8BE) * 7, false)
-    let result = i2cbuf[0] << 8;
-    result |= i2cbuf[1];
-    //basic.pause(10)
-    return result
-
-}
-
-
-
+/**
+ * get a reg
+ */
 function getReg(reg: number): number {
     pins.i2cWriteNumber(APDS9960_ADDRESS, reg, NumberFormat.UInt8BE);
     return pins.i2cReadNumber(APDS9960_ADDRESS, NumberFormat.UInt8BE);
-    
 }
 
 
-    /**
-     * Power On
-     */
-
-function PowerOn() {
-    let t = getReg(APDS9960_ENABLE)
-    t &= 0b00000001
-    setReg(APDS9960_ENABLE, t)
-    basic.pause(3)
-}
-    /**
-     * ALS Enable
-     * @param en is enable/disable ALS, eg: true
-     */
-
-function ALSEnable(en: boolean = true) {
-    let t = getReg(APDS9960_ENABLE)
-    t &= 0b00000010
-    //if (en) t |= 2
-    setReg(APDS9960_ENABLE, t)
-    basic.pause(10)
-}
-function WaitEnable(en: boolean = true) {
-    let t = getReg(APDS9960_ENABLE)
-    t &= 0b00001000
-    //if (en) t |= 4
-    setReg(APDS9960_ENABLE, t)
-    basic.pause(10)
-}
-
-    /**
-     * Initialize
-     */
-    //% blockId="APDS9960_INIT" block="APDS9960 Initialize"
-    //% weight=210 blockGap=8
-function init() {
-    PowerOn()
-    ALSEnable()
-    setReg(APDS9960_ENABLE, 0)
-    setReg(APDS9960_ATIME, 0xFF)
-    setReg(APDS9960_WTIME, 0xFF)
-    setReg(APDS9960_PERS, 0)
-    //setReg(APDS9960_CONFIG1, 0X40)
-    setReg(APDS9960_CONTROL, 0)  
-    //WaitEnable(true)
-    basic.pause(10)
+function getReg_int(reg: number): number {
+    pins.i2cWriteNumber(APDS9960_ADDRESS, reg, NumberFormat.UInt8BE);
+    return pins.i2cReadNumber(APDS9960_ADDRESS, NumberFormat.Int8LE);
 }
 
 /**
-* APDS9930 mudule
-*/
-init();
-
-basic.forever(()=>{
-    let l = getReg(APDS9960_ENABLE);
-    let m = getReg(APDS9960_STATUS);
-    let c = getReg(APDS9960_CDATAL);
-    let r = getReg(APDS9960_RDATAL);
-    let g = getReg(APDS9960_GDATAL);
-    let b = getReg(APDS9960_BDATAL);
-    
+ * get two reg, UInt16LE format
+ */
+function get2Reg(reg: number): number {
+    pins.i2cWriteNumber(APDS9960_ADDRESS, reg, NumberFormat.UInt8BE);
+    return pins.i2cReadNumber(APDS9960_ADDRESS, NumberFormat.Int16BE);
+}
 
 
-    //serial.writeNumber(r)
-    //basic.pause(100)
+
+function PowerOn() {
+    let t = getReg(APDS9960_ENABLE)
+    t |= 1
+    setReg(APDS9960_ENABLE, t)
+    basic.pause(3)
+}
+
+function ATIME(v: number) {
+    setReg(APDS9960_ATIME, v);
+}
+
+/**
+ * Power Off
+ */
+//% blockId="APDS9930_OFF" block="Power Off"
+//% weight=80 blockGap=8
+function PowerOff() {
+    let t = getReg(APDS9960_ENABLE)
+    t &= 0xFE
+    setReg(APDS9960_ENABLE, t)
+}
+
+/**
+ * ALS Enable
+ * @param en is enable/disable ALS, eg: true
+ */
+//% blockId="APDS9930_ALS_ENABLE" block="ALS Enable %en"
+//% weight=120 blockGap=8
+function ALSEnable(en: boolean = true) {
+    let t = getReg(APDS9960_ENABLE)
+    t &= 0x13
+    if (en) t |= 19
+    setReg(APDS9960_ENABLE, t)
+}
+
+function GAIN(en: boolean = true) {
+    let t = getReg(APDS9960_CONTROL)
+    t &= 0xFD
+    if (en) t |= 2
+    setReg(APDS9960_CONTROL, t)
+}
 
 
-    //}
-    /* This only uses RGB ... how can we integrate clear or calculate lux */
-    /* based exclusively on clear since this might be more reliable?      */
-    //let illuminance = (-0.32466 * r) + (1.57837 * g) + (-0.73191 * b);
-    basic.showNumber(l)
-    //basic.pause(1000)
-    //basic.showNumber(g)
-})
-    
+/**
+ * Wait Enable
+ * @param en is enable/disable wait timer, eg: true
+ */
+function PERS_REG(en: boolean = true) {
+    let t = getReg(APDS9960_PERS)
+    t &= 0x02
+    if (en) t |= 1
+    setReg(APDS9960_PERS, t)
+}
 
+function init() {
+    //ATIME(256 - 8)
+    //setReg(APDS9960_ENABLE, 0)
+    setReg(APDS9960_ATIME, 0xFF)
+    basic.pause(10)
+    setReg(APDS9960_WTIME, 0xFF)
+    //setReg(APDS9960_PERS, 0x22)
+    basic.pause(10)
+    setReg(APDS9960_CONFIG1, 0X40)
+    //setReg(APDS9930_PPULSE, 8)
+    //setReg(APDS9960_CONTROL, 0x2C)
+    basic.pause(10)
+    PERS_REG(true)
+    basic.pause(10)
+    GAIN(true);
+    basic.pause(10)
+    ALSEnable(true)
+    basic.pause(10)
+    PowerOn();
+    //WaitEnable(true)
+}
+//% color=#4c6ef5 weight=25 icon="\uf043" block="SHT3x Sensor"
+namespace CIP_APDS9960 {
+    let illuminance = 0
+    init();
+    /**
+     * Returns a number describing the lux 
+    */
+    //% blockId="APDS9960_read_LUX"
+    //% block="leer lux"
+    export function leer_lux(): number {
+        //let G = getReg(APDS9960_CONTROL)
+        let TL = get2Reg(APDS9960_AILTIL);
+        let TH = get2Reg(APDS9960_AIHTH);
+        let LH = get2Reg(APDS9960_AILTH);
+        let HL = get2Reg(APDS9960_AIHTL);
+        //let l = getReg(APDS9960_STATUS);
+        let c = get2Reg(APDS9960_CDATAL);
+        basic.pause(10)
+        if ((c >= TH + LH) || (c <= TL + HL)) {
+            let r = get2Reg(APDS9960_RDATAL);
+            let g = get2Reg(APDS9960_GDATAL);
+            let b = get2Reg(APDS9960_BDATAL);
+            illuminance = (-0.32466 * r) + (1.57837 * g) + (-0.73191 * b);
+            illuminance = illuminance/ 255
+            if (illuminance < 0) illuminance = Math.abs(illuminance)
+        }
+        return illuminance
+
+    }
+}
