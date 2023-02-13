da13968ce864f6d2604fab5c3656de8e9708dc99
diegogalv <123975129+diegogalv@users.noreply.github.com>
2023-02-13 16:48:08 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -132,7 +132,7 @@ namespace CIP_APDS9960 {
         let LH = get2Reg_lux(APDS9960_AILTH);
         let HL = get2Reg_lux(APDS9960_AIHTL);
         let l = get2Reg_lux(APDS9960_STATUS);
-        let c = get2Reg_lux(APDS9960_RDATAH);
+        let c = get2Reg_lux(APDS9960_RDATAL);
         basic.pause(10)
         if ((c >= TH + LH) || (c <= TL + HL)) {
             let r = get2Reg_lux(APDS9960_RDATAL);
