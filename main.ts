81517244d07fd23824d58b7ab30f0196baa6f180
diegogalv <123975129+diegogalv@users.noreply.github.com>
2023-02-13 15:53:08 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -72,7 +72,7 @@ function PowerOn() {
 function ALSEnable(en: boolean = true) {
     let t = get_Reg_lux(APDS9960_ENABLE)
     t &= 0B11101110
-    if (en) t |= 11
+    if (en) t |= 16
     set_Reg_lux(APDS9960_ENABLE, t)
     basic.pause(3)
 }
