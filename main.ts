12bae6f55211ebef6975dfd323ab124c49f09cf5
diegogalv <123975129+diegogalv@users.noreply.github.com>
2023-02-13 14:45:56 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -62,7 +62,7 @@ function get2Reg_lux(reg: number): number {
 
 
 function PowerOn() {
-    let t = get_Reg_lux(APDS9960_ENABLE)
+    let t = get2Reg_lux(APDS9960_ENABLE)
     t |= 1
     set_Reg_lux(APDS9960_ENABLE, t)
     basic.pause(3)
