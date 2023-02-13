ff5d09d1ada91ba1d89748c4e66ccdf687d988bc
diegogalv <123975129+diegogalv@users.noreply.github.com>
2023-02-13 15:32:10 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -63,7 +63,7 @@ function get2Reg_lux(reg: number): number {
 
 function PowerOn() {
     let t = get_Reg_lux(APDS9960_ENABLE)
-    t |= 0
+    t |= 1
     set_Reg_lux(APDS9960_ENABLE, t)
     basic.pause(3)
 }
@@ -71,8 +71,8 @@ function PowerOn() {
 
 function ALSEnable(en: boolean = true) {
     let t = get_Reg_lux(APDS9960_ENABLE)
-    t &= 0x03
-    if (en) t |= 5
+    t &= 0xFD
+    if (en) t |= 19
     set_Reg_lux(APDS9960_ENABLE, t)
     basic.pause(3)
 }
