4df385b81e13298ba3da66fdf0f0670478aada85
diegogalv <123975129+diegogalv@users.noreply.github.com>
2023-02-13 15:35:51 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -57,7 +57,7 @@ function getReg_int(reg: number): number {
 function get2Reg_lux(reg: number): number {
     pins.i2cWriteNumber(APDS9960_ADDRESS, reg, NumberFormat.UInt8BE);
     basic.pause(3)
-    return pins.i2cReadNumber(APDS9960_ADDRESS, NumberFormat.Int16BE);
+    return pins.i2cReadNumber(APDS9960_ADDRESS, NumberFormat.UInt16BE);
 }
 
 
@@ -132,7 +132,7 @@ namespace CIP_APDS9960 {
         let TH = get2Reg_lux(APDS9960_AIHTH);
         let LH = get2Reg_lux(APDS9960_AILTH);
         let HL = get2Reg_lux(APDS9960_AIHTL);
-        let l = get_Reg_lux(APDS9960_ENABLE);
+        let l = get2Reg_lux(APDS9960_ENABLE);
         let c = get2Reg_lux(APDS9960_CDATAL);
         basic.pause(10)
         if ((c >= TH + LH) || (c <= TL + HL)) {
